6b5268a784ec243444a4ffdc6ffc53c89ee3344b
CoderSid063 <sudhansupanda063@gmail.com>
2024-06-09 16:14:28 +0530

    feat : add backend

---
+++ app/api/users/signup/route.js
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { sendEmail } from "@/helpers/mailer";
+
+connect();
+
+export async function POST(request) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+
+    console.log(reqBody);
+
+    //check if user already exists
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    //hash password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+
+    //send verification email
+
+    await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id });
+
+    return NextResponse.json({
+      message: "User created successfully",
+      success: true,
+      savedUser,
+    });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
