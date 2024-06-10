7d07192474c8ffd55f5b53674c19dbd9e1d70d6c
CoderSid063 <sudhansupanda063@gmail.com>
2024-06-10 07:43:57 +0530

    feat : add midleware

---
+++ middleware.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+// Middleware function to handle routing based on user authentication
+export function middleware(request) {
+  const path = request.nextUrl.pathname; // Get the current request path
+
+  // Define public paths that do not require authentication
+  const isPublicPath =
+    path === "/login" || path === "/signup" || path === "/verifyemail";
+
+  // Retrieve the token from cookies
+  const token = request.cookies.get("token")?.value || "";
+
+  // If the user is authenticated and tries to access a public path, redirect to home page
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+
+  // If the user is not authenticated and tries to access a protected path, redirect to login page
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+
+  // If the path and authentication status are valid, allow the request to proceed
+}
+
+// Configuration for the middleware, specifying which paths to match
+export const config = {
+  matcher: ["/", "/profile", "/login", "/signup", "/verifyemail"], // Paths that this middleware should apply to
+};
